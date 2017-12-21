23152a933cf5c311926647c4ae925183c653e9bb
Roberto Sousa <beto.sousa22@gmail.com>
2017-12-21 16:52:27 +0000

    get the movie list using react redux on the move component

--- src/components/movies/movies.tsx
+++ src/components/movies/movies.tsx
@@ -1,39 +1,64 @@
 
 import * as React from 'react';
+import { connect } from "react-redux";
 
 // assets
 import './movies.css';
 
+import { getTopRatedMovies } from "../../actions/movies/movies"
+
+
+// movie interface
+export interface IMovie {
+	id: number;
+	title: string;
+	overview?: string;
+}
+
+@connect((store) => {
+	return {
+		moviesList: store.movies.movies.results,
+	};
+})
+
 // class
 class Movies extends React.Component <any, any> {
 
-
-	state = {
-		sampleState: ''
-	};
-
 	constructor(props: any) {
 		super(props);
-		console.log('constructor');// roberto
-	}
-
-	componentWillMount() {
-		console.log('componentWillMount');// roberto
 	}
 
 	componentDidMount() {
-		console.log('componentDid Mount');// roberto
+		try {
+			this.props.dispatch(getTopRatedMovies());
+		} catch (e) {
+			console.error('error-->', e);
+		}
 	}
 
 	render() {
+		const { moviesList } = this.props;
+		console.log('moviesList-->', moviesList);// roberto
 		return (
 			<div>
-				<h1>
-					{this.props.movie.title}
-				</h1>
-				<h2>
-					{this.props.movie.overview ? this.props.movie.overview : 'sample'}
-				</h2>
+				<p>
+					{
+						moviesList &&
+						moviesList.length
+					}
+				</p>
+				<div>
+					{
+						moviesList &&
+						moviesList.map((movie: IMovie)=> {
+							return(
+								<h1>
+									{movie.title}
+								</h1>
+							)
+						})
+					}
+				</div>
 			</div>
 		);
 	}
