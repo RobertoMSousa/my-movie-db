394cc3503a275d92fd5584ce7f141f2c772fcf91
Roberto Sousa <beto.sousa22@gmail.com>
2017-12-21 16:51:14 +0000

    clean up the code and pass all the movie logic into the movie component

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,22 @@
 
 import * as React from 'react';
 
-import './App.css';
+//
+// const middleware = applyMiddleware()
 
+import './App.css';
+import store from "./store"
 
 
 import Movie from './components/movies/movies';
+
 const logo = require('./logo.svg');
 
-interface IMovie {
-	id: number;
-	title: string;
-	overview?: string;
-}
-
-
-export const getData = () => ({
-	// 'https://api.themoviedb.org/3/movie/top_rated?api_key=' + process.env.REACT_APP_API_KEY + '&language=en-US&page=1'
-});
 
 class App extends React.Component <any, any> {
 
-
-	state = {
-		moviesList: []
-	}
-
 	constructor(props: any) {
 		super(props);
-		console.log('constructor');// roberto
-	}
-    //
-	// componentWillMount() {
-	// 	console.log('componentWillMount');// roberto
-	// }
-
-
-	componentDidMount() {
-		try {
-			console.log('componentDid Mount-->', process.env);// roberto
-			getData();
-			// const res = await fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=' + process.env.REACT_APP_API_KEY + '&language=en-US&page=1');
-			// const moviesListSample: any  =  await res.json();
-			// console.log('moviesListSample-->', moviesListSample);// roberto
-			// console.log('moviesListSample result-->', moviesListSample.results);// roberto
-			// if (moviesListSample && moviesListSample.results)
-			// {
-			// 	this.setState({
-			// 		moviesList: moviesListSample.results
-			// 	});
-			// }
-		} catch (e) {
-			console.error('error-->', e);
-			throw new Error('  ');
-		}
 	}
 
 	render() {
@@ -62,21 +25,11 @@ class App extends React.Component <any, any> {
 				<div className="App-header">
 			 		<img src={logo} className="App-logo" alt="logo" />
 				</div>
-				<div>
-					<p>
-					{this.state.moviesList.length}
-					</p>
-					{
-						this.state.moviesList.length > 0 &&
-						this.state.moviesList.map((movie: IMovie)=> {
-						return(
-							<Movie key={movie.id} movie={movie} />
-						)
-					})}
-				</div>
+				<Movie store={store}/>
 			</div>
 		);
 	}
 }
+
 
 export default App;
