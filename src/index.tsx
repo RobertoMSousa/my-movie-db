62fc73f94c7dad239c4f630e38b4b94147ac8730
Roberto Sousa <beto.sousa22@gmail.com>
2017-12-21 16:50:36 +0000

    import the redux store

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,17 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from "react-redux"
 import './index.css';
+import store from "./store"
+
+
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
+	<div>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</div>, document.getElementById('root') as HTMLElement);
+
 registerServiceWorker();
