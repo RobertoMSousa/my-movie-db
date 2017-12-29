eaac0597bdb66b1d98009f12ae4e3e4ec3e3e3a3
Roberto Sousa <beto.sousa22@gmail.com>
2017-12-29 18:21:50 +0000

    reduce for the movie request

--- src/reducers/movies/movies.ts
+++ src/reducers/movies/movies.ts
@@ -3,6 +3,7 @@
 // movies reducers
 export default function reducer(state = {
 	movies: [],
+	movie: {},
 	fetching: false,
 	fetched: false,
 	error: null,
@@ -16,6 +17,15 @@ export default function reducer(state = {
 			return { ...state, fetching: false, fetched: true, movies: action.payload }
 		}
 		case "GET_TOP_RATED_MOVIES_FAILED": {
+			return { ...state, fetching: false, fetched: false, error: action.payload }
+		}
+		case "GET_MOVIE_DATA": {
+			return { ...state, fetching: true }
+		}
+		case "GET_MOVIE_DATA_SUCCESS": {
+			return { ...state, fetching: false, fetched: true, movie: action.payload }
+		}
+		case "GET_MOVIE_DATA_FAILED": {
 			return { ...state, fetching: false, fetched: false, error: action.payload }
 		}
 	}
