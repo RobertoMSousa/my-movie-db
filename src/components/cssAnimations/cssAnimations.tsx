21c2aa28fd991483c3893af0ab19bb550078be31
Roberto Sousa <beto.sousa22@gmail.com>
2017-12-30 15:18:52 +0000

    complete the burger menu

--- src/components/cssAnimations/cssAnimations.tsx
+++ src/components/cssAnimations/cssAnimations.tsx
@@ -54,14 +54,34 @@ export default class CloseButton extends React.Component <any, any> {
 
 	render() {
 
-		// const arr = ['movies', 'shows', 'tv', 'music', 'others'];
+		const arr = ['movies', 'shows', 'tv', 'music', 'others'];
 
 		return (
-			<div id="closeButton" ref={this.setWrapperRef} className={this.state.open ? 'open': ''} onClick={this.toggleClass} >
-				<span></span>
-				<span></span>
-				<span></span>
+			<div ref={this.setWrapperRef} className={this.state.open ? 'open oppenned': 'open'} onClick={this.toggleClass} >
+				<span className="cls"></span>
+				<span>
+					<ul className="sub-menu">
+						{
+						arr.map((value: string)=> {
+							return(
+								<li>
+									<a>{value.toUpperCase()}</a>
+								</li>
+							);
+						})
+						}
+					</ul>
+				</span>
+				<span className="cls"></span>
 			</div>
 		);
 	}
 }
+
+
+
+// <div id="closeButton" ref={this.setWrapperRef} className={this.state.open ? 'open': ''} onClick={this.toggleClass} >
+// 	<span></span>
+// 	<span></span>
+// 	<span></span>
+// </div>
