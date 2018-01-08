79da8f634d8ae746c23abed442b2a2aa659bc831
Roberto Sousa <beto.sousa22@gmail.com>
2018-01-08 21:44:24 +0000

    load the most popular movies and display random on the top

--- src/components/landingPage/landingPage.tsx
+++ src/components/landingPage/landingPage.tsx
@@ -4,11 +4,17 @@ import { connect } from "react-redux";
 
 
 
+import * as Movie from '../../interfaces/movie';
+const imagePath: string = 'https://image.tmdb.org/t/p/w1920';
+
 import TopMenu from '../top-menu/top-menu';
+import './landingPage.css';
+import { getMostPopularMovies } from "../../actions/movies/movies";
 // assets
 
 @connect((store) => {
 	return {
+		movies: store.movies.movies.results,
 	};
 })
 
@@ -21,14 +27,31 @@ class LandingPage extends React.Component <any, any> {
 		};
 	}
 
+	componentDidMount() {
+		try {
+			this.props.dispatch(getMostPopularMovies());
+		} catch (e) {
+			console.error('error-->', e);
+		}
+	}
+
 	render() {
+		const movies: Array<Movie.IMovie> = this.props.movies;
 		return (
 			<div>
 				<TopMenu path={this.props.route.path}/>
-				<h1>Landing Page</h1>
+				{
+					movies &&
+					<div className='landigPage_top_movie_teaser' style={{backgroundImage : `url(${imagePath + movies[Math.floor(Math.random() * Math.floor(20))].backdrop_path})`}}></div>
+				}
 			</div>
 		);
 	}
 }
+
+//
+// {
+// 	movies && <img className='landigPage_top_movie_teaser' src={imagePath + movies[Math.floor(Math.random() * Math.floor(20))].backdrop_path} alt=''/>
+// }
 
 export default LandingPage;
