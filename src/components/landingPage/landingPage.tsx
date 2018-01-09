e4a15ece65fcaead42096eee2efa2392a4da5a2a
Roberto Sousa <beto.sousa22@gmail.com>
2018-01-09 23:05:04 +0000

    implement some new parts of the landing page

--- src/components/landingPage/landingPage.tsx
+++ src/components/landingPage/landingPage.tsx
@@ -6,10 +6,12 @@ import { connect } from "react-redux";
 
 import * as Movie from '../../interfaces/movie';
 const imagePath: string = 'https://image.tmdb.org/t/p/w1920';
+const posterPath: string = 'https://image.tmdb.org/t/p/w342';
 
 import TopMenu from '../top-menu/top-menu';
 import './landingPage.css';
 import { getMostPopularMovies } from "../../actions/movies/movies";
+const appBackGround = require('../../img/MobileApps.png');
 // assets
 
 @connect((store) => {
@@ -38,7 +40,7 @@ class LandingPage extends React.Component <any, any> {
 	render() {
 		const movies: Array<Movie.IMovie> = this.props.movies;
 		return (
-			<div>
+			<div className='landigPage_main_div'>
 				<TopMenu path={this.props.route.path}/>
 				{
 					movies &&
@@ -51,6 +53,37 @@ class LandingPage extends React.Component <any, any> {
 						</div>
 					</div>
 				}
+				{
+					movies &&
+					<div className='landigPage_call_to_action'>
+						<div className='landigPage_call_to_action_left'>
+							<div className='landigPage_call_to_action_left_title'>
+								<span>Lorem ipsum dolor sit amet, consectetur.</span>
+							</div>
+							<div className='landigPage_call_to_action_left_text'>
+								<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tristique magna sit amet purus gravida quis. Purus sit amet luctus venenatis lectus. In ante metus dictum at tempor commodo ullamcorper a lacus. Nulla pellentesque dignissim enim sit amet venenatis urna. Adipiscing bibendum est ultricies integer quis auctor elit sed vulputate.</span>
+							</div>
+							<div className='landigPage_register-button'>
+								<span className='landigPage_register-button_text'>Join us!</span>
+							</div>
+						</div>
+						<div className='landigPage_call_to_action_right'>
+							<div className='landigPage_call_to_action_right_movie_poster' style={{backgroundImage : `url(${posterPath + movies[Math.floor(Math.random() * Math.floor(20))].poster_path})`}}></div>
+							<div className='landigPage_call_to_action_right_movie_poster_span'>
+								<span>Lorem ipsum dolor sit amet</span>
+							</div>
+						</div>
+					</div>
+				}
+				<div className='landingPage_download_apps_container' style={{backgroundImage : `url(${appBackGround})`} }>
+					<div className='landingPage_download_apps_header' >
+						<span>Download our apps</span>
+					</div>
+					<div className='landingPage_download_apps_android'>
+					</div>
+					<div className ='landingPage_download_apps_ios'>
+					</div>
+				</div>
 			</div>
 		);
 	}
