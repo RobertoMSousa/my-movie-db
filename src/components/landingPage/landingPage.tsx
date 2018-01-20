ba82ddabfe9c6fdfe0964a6ccb81a54f2309eaf1
Roberto Sousa <beto.sousa22@gmail.com>
2018-01-20 12:37:42 +0000

    ts lint some files

--- src/components/landingPage/landingPage.tsx
+++ src/components/landingPage/landingPage.tsx
@@ -1,20 +1,25 @@
 
 import * as React from 'react';
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
 import isEmail from 'validator/lib/isEmail';
 import swal from 'sweetalert';
 
-
+// interfaces
 import * as Movie from '../../interfaces/movie';
-const imagePath: string = 'https://image.tmdb.org/t/p/w1920';
-const posterPath: string = 'https://image.tmdb.org/t/p/w342';
 
+// external components
 import TopMenu from '../top-menu/top-menu';
 import TextArea from '../textArea/textArea';
-import './landingPage.css';
-import { getMostPopularMovies } from "../../actions/movies/movies";
 
-// assets
+// css
+import './landingPage.css';
+
+import { getMostPopularMovies } from '../../actions/movies/movies';
+import { subscribe_newletter } from '../../actions/newsletter/newsletter';
+
+// static assets
+const imagePath: string = 'https://image.tmdb.org/t/p/w1920';
+const posterPath: string = 'https://image.tmdb.org/t/p/w342';
 const appBackGround = require('../../img/MobileApps.png');
 const appStoreLogo = require('../../img/app-store.svg');
 const googlePlayLogo = require('../../img/google-play.svg');
@@ -31,7 +36,7 @@ const googleLogo = require('../../img/google.svg');
 // class
 class LandingPage extends React.Component <any, any> {
 
-	constructor(props) {
+	constructor(props: any) {
 		super(props);
 		this.state = {
 			email: ''
@@ -50,103 +55,100 @@ class LandingPage extends React.Component <any, any> {
 
 	handleSubmit() {
 		try {
-			if (isEmail(this.state.email)){
-			swal("Good job!", "Thanks for Subscribing!", "success");
-			/*
-			TODO: send the email to the backend
-			*/
-		}
-			else {
-				swal("Uppps!", "The email is not valid!", "error");
+			if (isEmail(this.state.email)) {
+				swal('Good job!', 'Thanks for Subscribing!', 'success');
+				this.props.dispatch(subscribe_newletter(this.state.email));
+			} else {
+				swal('Uppps!', 'The email is not valid!', 'error');
 			}
 		} catch (e) {
 			console.log('error-->', e);
-			throw new Error('  ');
+			throw new Error('');
 		}
 	}
 
-	handleEmail(email) {
+	handleEmail(email: string) {
 		this.setState({ email: email });
 	}
 
 	render() {
 		const movies: Array<Movie.IMovie> = this.props.movies;
 		return (
-			<div className='landigPage_main_div'>
+			<div className="landigPage_main_div">
 				<TopMenu path={this.props.route.path}/>
 				{
 					movies &&
-					<div className='landigPage_top_movie_teaser' style={{backgroundImage : `url(${imagePath + movies[Math.floor(Math.random() * Math.floor(20))].backdrop_path})`}}>
-						<div className='landigPage_top_title_wrapper'>
-							<span className='landigPage_top_title'>Lorem ipsum dolor sit amet</span>
+					<div className="landigPage_top_movie_teaser" style={{backgroundImage : `url(${imagePath + movies[Math.floor(Math.random() * Math.floor(20))].backdrop_path})`}}>
+						<div className="landigPage_top_title_wrapper">
+							<span className="landigPage_top_title">Lorem ipsum dolor sit amet</span>
 						</div>
-						<div className='landigPage_register-button'>
-						 	<span className='landigPage_register-button_text'>Join us!</span>
+						<div className="landigPage_register-button">
+							<span className="landigPage_register-button_text">Join us!</span>
 						</div>
 					</div>
 				}
 				{
 					movies &&
-					<div className='landigPage_call_to_action'>
-						<div className='landigPage_call_to_action_left'>
-							<div className='landigPage_call_to_action_left_title'>
+					<div className="landigPage_call_to_action">
+						<div className="landigPage_call_to_action_left">
+							<div className="landigPage_call_to_action_left_title">
 								<span>Lorem ipsum dolor sit amet, consectetur.</span>
 							</div>
-							<div className='landigPage_call_to_action_left_text'>
+							<div className="landigPage_call_to_action_left_text">
 								<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tristique magna sit amet purus gravida quis. Purus sit amet luctus venenatis lectus. In ante metus dictum at tempor commodo ullamcorper a lacus. Nulla pellentesque dignissim enim sit amet venenatis urna. Adipiscing bibendum est ultricies integer quis auctor elit sed vulputate.</span>
 							</div>
-							<div className='landigPage_register-button'>
-								<span className='landigPage_register-button_text'>Join us!</span>
+							<div className="landigPage_register-button">
+								<span className="landigPage_register-button_text">Join us!</span>
 							</div>
 						</div>
-						<div className='landigPage_call_to_action_right'>
-							<div className='landigPage_call_to_action_right_movie_poster' style={{backgroundImage : `url(${posterPath + movies[Math.floor(Math.random() * Math.floor(20))].poster_path})`}}></div>
-							<div className='landigPage_call_to_action_right_movie_poster_span'>
+						<div className="landigPage_call_to_action_right">
+							<div className="landigPage_call_to_action_right_movie_poster" style={{backgroundImage : `url(${posterPath + movies[Math.floor(Math.random() * Math.floor(20))].poster_path})`}}/>
+							<div className="landigPage_call_to_action_right_movie_poster_span">
 								<span>Lorem ipsum dolor sit amet</span>
 							</div>
 						</div>
 					</div>
 				}
-				<div className='landingPage_download_apps_container' style={{backgroundImage : `url(${appBackGround})`} }>
-					<div className='landingPage_download_apps_header' >
+				<div className="landingPage_download_apps_container" style={{backgroundImage : `url(${appBackGround})`} }>
+					<div className="landingPage_download_apps_header">
 						<span>Download our apps</span>
 					</div>
-					<div className='landingPage_download_apps_android'>
+					<div className="landingPage_download_apps_android">
 						<img src={googlePlayLogo}  className="landingPage_download_apps_logo" alt="" />
 					</div>
-					<div className ='landingPage_download_apps_ios'>
+					<div className="landingPage_download_apps_ios">
 						<img src={appStoreLogo} className="landingPage_download_apps_logo" alt="" />
 					</div>
 				</div>
-				<div className='landingPage_subscribe_containter'>
-					<div className='landingPage_subscribe_title'>
+				<div className="landingPage_subscribe_containter">
+					<div className="landingPage_subscribe_title">
 						<span>Subscribe to stay updated</span>
 					</div>
-					<div className='landingPage_subscribe_email_container'>
-						<div className='landingPage_subscribe_email'>
-							<TextArea placeholder='email address' callbackFromParent={this.handleEmail} backgroundColor='#d8d8d8'/>
+					<div className="landingPage_subscribe_email_container">
+						<div className="landingPage_subscribe_email">
+							<TextArea placeholder="email address" callbackFromParent={this.handleEmail} backgroundColor="#d8d8d8"/>
 						</div>
-						<div onClick={this.handleSubmit} className='landingPage_subscribe_submit'>
+						<div onClick={this.handleSubmit} className="landingPage_subscribe_submit">
 							<span>Submit</span>
 						</div>
 					</div>
 				</div>
-				<div className='landingPage_footer_containter'>
-					<div className='landingPage_footer_links'>
+				<div className="landingPage_footer_containter">
+					<div className="landingPage_footer_links">
 						<span>About</span>
 						<span>API</span>
 						<span>Terms</span>
 						<span>Contact</span>
 					</div>
-					<div className='landingPage_footer_social'>
-						<div className='landingPage_footer_social_title'>Follow Us</div>
-						<div className='landingPage_footer_social_twitter'>
+					<div className="landingPage_footer_social">
+						<div className="landingPage_footer_social_title">Follow Us</div>
+						<div className="landingPage_footer_social_twitter">
 							<img src={twitterLogo} className="" alt="" />
 						</div>
-						<div className='landingPage_footer_social_facebook'>
+						<div className="landingPage_footer_social_facebook">
 							<img src={facebookLogo} className="" alt="" />
 						</div>
-						<div className='landingPage_footer_social_google'>
+						<div className="landingPage_footer_social_google">
 							<img src={googleLogo} className="" alt="" />
 						</div>
 					</div>
@@ -155,6 +157,5 @@ class LandingPage extends React.Component <any, any> {
 		);
 	}
 }
-
 
 export default LandingPage;
--- src/reducers/movies/movies.ts
+++ src/reducers/movies/movies.ts
@@ -1,42 +1,46 @@
 
-
 // movies reducers
-export default function reducer(state = {
+
+export default function reducer(
+	state: any= {
 	movies: [],
 	movie: {},
 	fetching: false,
 	fetched: false,
 	error: null,
-}, action) {
+	}, 
+	action: any) {
 	switch (action.type) {
-		case "GET_TOP_RATED_MOVIES": {
-			return { ...state, fetching: true }
+		case 'GET_TOP_RATED_MOVIES': {
+			return { ...state, fetching: true };
 		}
-		case "GET_TOP_RATED_MOVIES_SUCCESS": {
-			return { ...state, fetching: false, fetched: true, movies: action.payload }
+		case 'GET_TOP_RATED_MOVIES_SUCCESS': {
+			return { ...state, fetching: false, fetched: true, movies: action.payload };
 		}
-		case "GET_TOP_RATED_MOVIES_FAILED": {
-			return { ...state, fetching: false, fetched: false, error: action.payload }
+		case 'GET_TOP_RATED_MOVIES_FAILED': {
+			return { ...state, fetching: false, fetched: false, error: action.payload };
+		}	
+		case 'GET_MOST_POPULAR_MOVIES': {
+			return { ...state, fetching: true };
 		}
-		case "GET_MOST_POPULAR_MOVIES": {
-			return { ...state, fetching: true }
+		case 'GET_MOST_POPULAR_MOVIES_SUCCESS': {
+			return { ...state, fetching: false, fetched: true, movies: action.payload };
 		}
-		case "GET_MOST_POPULAR_MOVIES_SUCCESS": {
-			return { ...state, fetching: false, fetched: true, movies: action.payload }
+		case 'GET_MOST_POPULAR_MOVIES_FAILED': {
+			return { ...state, fetching: false, fetched: false, error: action.payload };
 		}
-		case "GET_MOST_POPULAR_MOVIES_FAILED": {
-			return { ...state, fetching: false, fetched: false, error: action.payload }
+		case 'GET_MOVIE_DATA': {
+			return { ...state, fetching: true };
 		}
-		case "GET_MOVIE_DATA": {
-			return { ...state, fetching: true }
+		case 'GET_MOVIE_DATA_SUCCESS': {
+			return { ...state, fetching: false, fetched: true, movie: action.payload };
 		}
-		case "GET_MOVIE_DATA_SUCCESS": {
-			return { ...state, fetching: false, fetched: true, movie: action.payload }
+		case 'GET_MOVIE_DATA_FAILED': {
+			return { ...state, fetching: false, fetched: false, error: action.payload };
 		}
-		case "GET_MOVIE_DATA_FAILED": {
-			return { ...state, fetching: false, fetched: false, error: action.payload }
+		default: {
+			return { ...state, fetching: false, fetched: false, error: 'fall on the default'};
 		}
 	}
-
-	return state
+	// return state;
 }
