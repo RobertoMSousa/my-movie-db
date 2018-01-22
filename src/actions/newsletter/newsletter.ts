4b0b0069000d5e9121e6e29b96b719db89318049
Roberto Sousa <beto.sousa22@gmail.com>
2018-01-22 19:42:48 +0000

    update the axios to use the new newsletter routes add some basic style

--- src/actions/newsletter/newsletter.ts
+++ src/actions/newsletter/newsletter.ts
@@ -1,5 +1,6 @@
 
 import axios from 'axios';
+import swal from 'sweetalert';
 
 const backendUrl: string = process.env.BACKEND_SERVER_URL ? <string> process.env.BACKEND_SERVER_URL : 'http://localhost:8080';
 
@@ -9,11 +10,13 @@ Subscribe to the newsletter
 export function subscribe_newletter(email: string) {
 	return function(dispatch: any) {
 		dispatch({ type: 'SUBSCRIBE_TO_NEWSLETTER'});
-		axios.post(backendUrl + '/user/newsletter', {'email': email})
+		axios.post(backendUrl + '/newsletter', {'email': email})
 			.then((response) => {
+				swal('Good job!', 'Thanks for Subscribing!', 'success');
 				dispatch({ type: 'SUBSCRIBE_TO_NEWSLETTER_SUCCESS', payload: response.data });
 			})
 			.catch((err) => {
+				swal('Uppps!', 'Something whent wrong!', 'error');
 				dispatch({ type: 'SUBSCRIBE_TO_NEWSLETTER_FAILED', payload: err });
 			});
 	};
@@ -25,11 +28,12 @@ Unsubscribe to the newsletter
 export function unsubscribe_newletter(email: string) {
 	return function(dispatch: any) {
 		dispatch({ type: 'SUBSCRIBE_TO_NEWSLETTER'});
-		axios.delete(backendUrl + '/user/newsletter', {'params': {'email': email}})
+		axios.delete(backendUrl + '/newsletter', {'params': {'email': email}})
 			.then((response) => {
 				dispatch({ type: 'SUBSCRIBE_TO_NEWSLETTER_SUCCESS', payload: response.data });
 			})
 			.catch((err) => {
+				swal('Uppps!', 'Something whent wrong!', 'error');
 				dispatch({ type: 'SUBSCRIBE_TO_NEWSLETTER_FAILED', payload: err });
 			});
 	};
