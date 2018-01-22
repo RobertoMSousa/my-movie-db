54e89efc5d3347e64606f9a8a56ff156110bd773
Roberto Sousa <beto.sousa22@gmail.com>
2018-01-22 19:35:54 +0000

    create the not found component

--- src/components/notFound/notFound.tsx
+++ src/components/notFound/notFound.tsx
@@ -1,11 +1,16 @@
 
 import * as React from 'react';
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
-
-
+// external components
 import TopMenu from '../top-menu/top-menu';
-// assets
+
+// css
+import './notFound.css';
+
+// static assets
+const notFound = require('../../img/404.svg');
 
 @connect((store) => {
 	return {
@@ -15,17 +20,25 @@ import TopMenu from '../top-menu/top-menu';
 // class
 class NotFound extends React.Component <any, any> {
 
-	constructor(props) {
+	constructor(props: any) {
 		super(props);
-		this.state = {
-		};
+		this.state = {};
 	}
 
 	render() {
 		return (
 			<div>
 				<TopMenu path={this.props.route.path}/>
-				<h1>Not Found</h1>
+				<div className="mainNotFoundContainer">
+					<div className="notFoundImage">
+						<img src={notFound} className="" alt=""/>
+					</div>
+
+				<Link className="return404Button noSelect remove_link_style" to="/">
+					<span>Return</span>
+				</Link>
+
+				</div>
 			</div>
 		);
 	}
