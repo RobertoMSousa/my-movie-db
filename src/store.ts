da534ba0195377915e37d27b461f7da36aaab401
Roberto Sousa <beto.sousa22@gmail.com>
2018-01-25 01:20:42 +0000

    basic lint changes

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,9 @@
-import reducer from "./reducers/index";
-import { createStore, applyMiddleware } from "redux";
+import reducer from './reducers/index';
+import { createStore, applyMiddleware } from 'redux';
 
 // import logger from "redux-logger";
-import thunk from "redux-thunk";
-import promise from "redux-promise-middleware";
+import thunk from 'redux-thunk';
+import promise from 'redux-promise-middleware';
 //
 // const middleware = applyMiddleware(promise(), logger());
 const middleware = applyMiddleware(promise(), thunk);
