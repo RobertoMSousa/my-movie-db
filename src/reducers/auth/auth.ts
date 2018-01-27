f2367b066ef161c2816b2a96ea1dbdac64e97075
Roberto Sousa <beto.sousa22@gmail.com>
2018-01-27 09:41:25 +0000

    create the user index and the auth reducer

--- src/reducers/auth/auth.ts
+++ src/reducers/auth/auth.ts
@@ -4,6 +4,8 @@
 export default function reducer(
 	state: any= {
 	message: {},
+	status: Number,
+	user: {},
 	fetching: false,
 	fetched: false,
 	error: null,
@@ -14,28 +16,28 @@ export default function reducer(
 			return { ...state, fetching: true };
 		}
 		case 'SIGNIN_USER_SUCCESS': {
-			return { ...state, fetching: false, fetched: true, message: action.payload };
+			return { ...state, fetching: false, fetched: true, message: action.payload, status: action.status };
 		}
 		case 'SIGNIN_USER_FAILED': {
-			return { ...state, fetching: false, fetched: false, error: action.payload };
+			return { ...state, fetching: false, fetched: false, error: action.payload, status: action.status };
 		}
 		case 'SIGNUP_USER': {
 			return { ...state, fetching: true };
 		}
 		case 'SIGNUP_USER_SUCCESS': {
-			return { ...state, fetching: false, fetched: true, message: action.payload };
+			return { ...state, fetching: false, fetched: true, message: action.payload, status: action.status };
 		}
 		case 'SIGNUP_USER_FAILED': {
-			return { ...state, fetching: false, fetched: false, error: action.payload };
+			return { ...state, fetching: false, fetched: false, error: action.payload, status: action.status };
 		}
 		case 'SIGNOUT_USER': {
 			return { ...state, fetching: true };
 		}
 		case 'SIGNOUT_USER_SUCCESS': {
-			return { ...state, fetching: false, fetched: true, message: action.payload };
+			return { ...state, fetching: false, fetched: true, message: action.payload, status: action.status };
 		}
 		case 'SIGNOUT_USER_FAILED': {
-			return { ...state, fetching: false, fetched: false, error: action.payload };
+			return { ...state, fetching: false, fetched: false, error: action.payload, status: action.status };
 		}
 		default: {
 			return { ...state, fetching: false, fetched: false, error: 'fall on the default of auth reducer'};
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -4,9 +4,11 @@ import { combineReducers } from 'redux';
 import movies from './movies/movies';
 import auth from './auth/auth';
 import newsletter from './newsletter/newsletter';
+import user from './user/user';
 
 export default combineReducers({
 	movies,
 	auth,
-	newsletter
+	newsletter,
+	user
 });
