a0a716f296565713d7b4e1676749f89683dfdb0e
Roberto Sousa <beto.sousa22@gmail.com>
2018-01-27 09:38:51 +0000

    create the routes on a different files

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+// import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-// components import 
-import LandingPage from './components/landingPage/landingPage';
-import Movies from './components/movies/moviesList';
-import Shows from './components/shows/shows';
-import Music from './components/music/music';
-import Others from './components/others/others';
-import SignIn from './components/auth/signin';
-import SignUp from './components/auth/signup';
-import NotFound from './components/notFound/notFound';
-import ComingSoon from './components/comingSoon/comingSoon';
+// import App from './App';
+import Routes from './routes';
+// import registerServiceWorker from './registerServiceWorker';
 
 // css code
 import './index.css';
@@ -28,21 +18,9 @@ const app = document.getElementById('root');
 ReactDOM.render(
 	<div className="indexMainContainer">
 		<Provider store={store}>
-			<Router history={hashHistory}>
-				<Route path="/" component={App}>
-					<IndexRoute component={LandingPage} />
-					<Route path="movies" name="movies" component={Movies}/>
-					<Route path="shows" name="shows" component={Shows}/>
-					<Route path="music" name="music" component={Music}/>
-					<Route path="others" name="others" component={Others}/>
-					<Route path="signin" name="signin" component={SignIn}/>
-					<Route path="signup" name="signup" component={SignUp}/>
-					<Route path="comingsoon" name="comingsoon" component={ComingSoon}/>
-					<Route path="*" name="notFound" component={NotFound}/>
-				</Route>
-			</Router>
+			<Routes/>
 		</Provider>
 	</div>,
 	app);
 
-registerServiceWorker();
+// registerServiceWorker();
---
+++ src/routes.tsx
@@ -0,0 +1,54 @@
+
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+
+// components import 
+import App from './App';
+import LandingPage from './components/landingPage/landingPage';
+import Movies from './components/movies/moviesList';
+import Shows from './components/shows/shows';
+import Music from './components/music/music';
+import Others from './components/others/others';
+import SignIn from './components/auth/signin';
+import SignUp from './components/auth/signup';
+import NotFound from './components/notFound/notFound';
+import ComingSoon from './components/comingSoon/comingSoon';
+import UserPage from './components/user/userPage';
+
+@connect((store) => {
+	return {
+		user: store.auth.user
+	};
+})
+
+// class
+class Routes extends React.Component <any, any> {
+
+	constructor(props: any) {
+		super(props);
+		this.state = {
+		};
+	}
+
+	render() {
+		return (
+			<Router history={hashHistory}>
+				<Route path="/" component={App}>
+					<IndexRoute component={LandingPage} />
+					<Route path="movies" name="movies" component={Movies}/>
+					<Route path="shows" name="shows" component={Shows}/>
+					<Route path="music" name="music" component={Music}/>
+					<Route path="others" name="others" component={Others}/>
+					<Route path="signin" name="signin" component={SignIn}/>
+					<Route path="signup" name="signup" component={SignUp}/>
+					<Route path="comingsoon" name="comingsoon" component={ComingSoon}/>
+					<Route path="user" name="user" component={UserPage}/>
+					<Route path="*" name="notFound" component={NotFound}/>
+				</Route>
+			</Router>
+		);
+	}
+}
+
+export default Routes;
