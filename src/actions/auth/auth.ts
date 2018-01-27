9b4080aeab42b92b23a0a6f338facb054c8fda64
Roberto Sousa <beto.sousa22@gmail.com>
2018-01-27 09:39:20 +0000

    update the dispatch types on the auth

--- src/actions/auth/auth.ts
+++ src/actions/auth/auth.ts
@@ -7,13 +7,13 @@ Sign up a new user
 */
 export function signup_user(email: string, password: string, passwordRepeated: string) {
 	return function(dispatch: any) {
-		dispatch({ type: 'SIGNIN_USER'});
+		dispatch({ type: 'SIGNUP_USER'});
 		axios.post(backendUrl + '/auth/signup', {'email': email, 'password': password, 'passwordRepeated': passwordRepeated})
 			.then((response) => {
-				dispatch({ type: 'SIGNIN_USER_SUCCESS', payload: response.data });
+				dispatch({ type: 'SIGNUP_USER_SUCCESS', status: response.status, payload: response.data });
 			})
 			.catch((err) => {
-				dispatch({ type: 'SIGNIN_USER_FAILED', payload: err });
+				dispatch({ type: 'SIGNUP_USER_FAILED', status: err.status, payload: err });
 			});
 	};
 }
@@ -23,13 +23,13 @@ Sign in user into platform
 */
 export function signin_user(email: string, password: string) {
 	return function(dispatch: any) {
-		dispatch({ type: 'SIGNUP_USER'});
+		dispatch({ type: 'SIGNIN_USER'});
 		axios.post(backendUrl + '/auth/login', {'email': email, 'password': password})
 			.then((response) => {
-				dispatch({ type: 'SIGNUP_USER_SUCCESS', payload: response.data });
+				dispatch({ type: 'SIGNIN_USER_SUCCESS',  status: response.status, payload: response.data });
 			})
 			.catch((err) => {
-				dispatch({ type: 'SIGNUP_USER_FAILED', payload: err });
+				dispatch({ type: 'SIGNIN_USER_FAILED', status: err.status, payload: err});
 			});
 	};
 }
@@ -42,10 +42,10 @@ export function signout_user(email: string, password: string) {
 		dispatch({ type: 'SIGNOUT_USER'});
 		axios.get(backendUrl + '/auth/logout')
 			.then((response) => {
-				dispatch({ type: 'SIGNOUT_USER_SUCCESS', payload: response.data });
+				dispatch({ type: 'SIGNOUT_USER_SUCCESS',  status: response.status, payload: response.data });
 			})
 			.catch((err) => {
-				dispatch({ type: 'SIGNOUT_USER_FAILED', payload: err });
+				dispatch({ type: 'SIGNOUT_USER_FAILED', status: err.status, payload: err });
 			});
 	};
 }
