09a2abc6618bd16e163cf09a60827dce9163d208
Roberto Sousa <beto.sousa22@gmail.com>
2018-01-31 18:19:57 +0000

    change the auth reducer

--- src/reducers/auth/auth.ts
+++ src/reducers/auth/auth.ts
@@ -3,41 +3,41 @@
 
 export default function reducer(
 	state: any= {
-	message: {},
 	status: Number,
-	user: {},
+	message: {},
 	fetching: false,
 	fetched: false,
 	error: null,
 	}, 
 	action: any) {
+	console.log('action-->', action); // roberto
 	switch (action.type) {
 		case 'SIGNIN_USER': {
 			return { ...state, fetching: true };
 		}
 		case 'SIGNIN_USER_SUCCESS': {
-			return { ...state, fetching: false, fetched: true, message: action.payload, status: action.status };
+			return { ...state, fetching: false, fetched: true, message: action.payload};
 		}
 		case 'SIGNIN_USER_FAILED': {
-			return { ...state, fetching: false, fetched: false, error: action.payload, status: action.status };
+			return { ...state, fetching: false, fetched: false, error: action.payload };
 		}
 		case 'SIGNUP_USER': {
 			return { ...state, fetching: true };
 		}
 		case 'SIGNUP_USER_SUCCESS': {
-			return { ...state, fetching: false, fetched: true, message: action.payload, status: action.status };
+			return { ...state, fetching: false, fetched: true, message: action.payload };
 		}
 		case 'SIGNUP_USER_FAILED': {
-			return { ...state, fetching: false, fetched: false, error: action.payload, status: action.status };
+			return { ...state, fetching: false, fetched: false, error: action.payload };
 		}
 		case 'SIGNOUT_USER': {
 			return { ...state, fetching: true };
 		}
 		case 'SIGNOUT_USER_SUCCESS': {
-			return { ...state, fetching: false, fetched: true, message: action.payload, status: action.status };
+			return { ...state, fetching: false, fetched: true, message: action.payload };
 		}
 		case 'SIGNOUT_USER_FAILED': {
-			return { ...state, fetching: false, fetched: false, error: action.payload, status: action.status };
+			return { ...state, fetching: false, fetched: false, error: action.payload };
 		}
 		default: {
 			return { ...state, fetching: false, fetched: false, error: 'fall on the default of auth reducer'};
