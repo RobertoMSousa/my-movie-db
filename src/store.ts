4bd464374f62f40ff4bf9b1150143421b1625ea4
Roberto Sousa <beto.sousa22@gmail.com>
2018-02-16 23:50:45 +0000

    make the store persist

--- src/store.ts
+++ src/store.ts
@@ -2,23 +2,21 @@ import reducer from './reducers/index';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
-import { persistStore } from 'redux-persist';
-
-// import storage from 'redux-persist/lib/storage';
-
+import { persistStore, persistReducer } from 'redux-persist';
+// import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 // import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
-// import rootReducer from './reducers'; // the value from combineReducers
 
 const middleware = applyMiddleware(promise(), thunk);
 
-export const store = createStore(reducer, middleware);
+const persistConfig = {
+	key: 'root',
+	storage,
+	whitelist: ['auth']
+};
 
-// const persistConfig = {
-// 	key: 'root',
-// 	storage: storage,
-// 	stateReconciler: autoMergeLevel2
-// };
+const persistedReducerLocal = persistReducer(persistConfig, reducer);
 
-// const persistReducerLocal = persistReducer(persistConfig, rootReducer);
+export const store = createStore(persistedReducerLocal, middleware);
 
 export const persistor = persistStore(store);
